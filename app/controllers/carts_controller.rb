2fd5c6186da14239ce77bc5d3f94ebf8a809edec
tRav3r <tRav3r@gmail.com>
2014-01-02 15:57:33 +0200

    Shopping cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,11 +13,16 @@ class CartsController < ApplicationController
   # GET /cards/1
   # GET /cards/1.json
   def show
-    @cart = Cart.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @cart }
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to acess invalid cart #{params[id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @cart }
+      end
     end
   end
 
